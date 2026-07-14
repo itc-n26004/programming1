b29ae567c3524cf44b50c3bebd17ef66d66a1a4e
itc-n26004 <n26004@std.it-college.ac.jp>
2026-07-14 02:42:28 +0000

    3-8

---
+++ chap03/q3_8_1.py
@@ -0,0 +1,8 @@
+import pickle
+with open('test1.pkl', 'wb') as f:
+    my_list = list(range(1,11))
+    pickle.dump(my_list, f)
+
+with open('test1.pkl', 'rb') as f:
+    loaded_list = pickle.load(f)
+    print(loaded_list)
